68db9a89bbf9d530caa2052b68298dd2481c85b0
Steven Starwalt <steven.starwalt@yahoo.com>
2018-03-25 17:19:01 -0700

    fixed the function

--- largest-product-array/largest_product_array.py
+++ largest-product-array/largest_product_array.py
@@ -2,12 +2,21 @@ def largest_product(list):
     
     max = list[0][0] * list[0][1]
 
-    for i in list -1:
-        for j in list[i] - 2:
-            product = list[i][j] * list[i][j + 1]
+    outer_list = [1] * len(list)
+    list_index = -1
+    
+    for i in outer_list:
+        list_index += 1
+        inner_list = [1] * len(list[list_index])
+        inner_index = -1
+        for j in inner_list:
+            inner_index += 1
+            if inner_index == len(inner_list) - 1:
+                break
+            product = list[list_index][inner_index] * list[list_index][inner_index + 1]
             if max < product:
-                max = product
+                max = product  
     return max            
 
 if __name__ == '__main__':
-    largest_product([[1,2], [2,3], [3,4], [1, 2, 1, 3]])
+    largest_product([[1,2], [2,3], [3,4], [1, 5, 4, 6]])
