1b2506be4a79bbf9ef67ea6c229187e704d2c6f2
Steven Starwalt <steven.starwalt@yahoo.com>
2018-04-02 14:08:27 -0700

    megan found an indentation error that I missed too many times

---
+++ linked_list/linked_list.py
@@ -0,0 +1,33 @@
+from .node import Node
+
+
+class Linked_List:
+    def __init__(self, iterable=[]):
+        self.head = None
+        self._size = 0
+
+        if not isinstance(iterable, (list, dict, tuple)):
+            """this checks if the iterable is a true iterable and inserts each value as a new node"""
+            raise TypeError('Iterable must be a list, dict, or tuple')   
+        for i in iterable:
+            self.insert(i)
+
+    def insert(self, val):
+        """This will add a node the front of the list"""
+        try:
+            node = Node(val)
+        except TypeError:
+            raise TypeError('Cannot insert a value of none')
+
+        node._next = self.head
+        self.head = node
+        self._size += 1
+
+        return self.head
+
+    def remove(self):
+        """remove the node at the head of the linked_list, decrement the ._size and    return the value"""
+        val = self.head
+        self.head = self.head._next
+        self._size -= 1
+        return val
