4cbbcd250f83ebcb3e23f5ddacb4fa14b846c9fc
Steven Starwalt <steven.starwalt@yahoo.com>
2018-04-04 16:58:24 -0700

    cleaned up the code, finished the readme

--- multi_bracket_validation/multi_bracket_validation.py
+++ multi_bracket_validation/multi_bracket_validation.py
@@ -13,17 +13,14 @@ def switch(char):
 
 
 def multi_bracket_validation(input):
-    """this will push any opening brackets into a stack, poping off the stack for every closing bracket, and verifing it matches tnhe last opening bracket pushed""" 
+    """this will push any opening brackets into a stack, poping off the stack for every closing bracket, and verifing it matches tnhe last opening"""
     stack = Stack()
-    new = ''
-    top = ''
+
     for char in input:
         if re.match(r"[([{]", char):
-            new = switch(char)
-            stack.push(new)
+            stack.push(switch(char))
         elif re.match(r"[)\]}]", char):
-            top = stack.pop()
-            if str(top) != char:
+            if str(stack.pop()) != char:
                 return False
     if stack.top is None:
         return True
