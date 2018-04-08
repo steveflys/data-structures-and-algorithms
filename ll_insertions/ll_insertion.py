fc7368eb5153e923dfbd5496a30536fcad43f550
Steven Starwalt <steven.starwalt@yahoo.com>
2018-04-08 16:33:33 -0700

    aded the code and now need to test

---
+++ ll_insertions/ll_insertion.py
@@ -0,0 +1,87 @@
+from .node import Node
+
+
+class Linked_List:
+    def __init__(self, iterable=[]):
+        self.head = None
+        self._size = 0
+
+        if not isinstance(iterable, (list, dict, tuple)):
+            """this checks if the iterable is a true iterable and inserts each value as a new node"""
+            raise TypeError('Iterable must be a list, dict, or tuple')   
+        for i in iterable:
+            self.insert(i)
+
+    def __repr__(self):
+        """this will return head and size of linked list when called"""
+        return '<head> => {}, <_size> => {}'.format(self.head.val, self._size)
+
+    def __len__(self):
+        """returns linked list length when called"""
+        return self._size
+
+    def insert(self, val):
+        """This will add a node the front of the list"""
+        try:
+            node = Node(val)
+        except TypeError:
+            raise TypeError('Cannot insert a value of none')
+
+        node._next = self.head
+        self.head = node
+        self._size += 1
+
+        return self.head
+
+    def append(self, value):
+        """this will add a new node to the end of the list"""
+        try:
+            node = Node(value)
+        except TypeError:
+            raise TypeError('Cannot insert a value of none')
+
+        current = self.head
+
+        while next:
+            current = current._next
+        current.next = node
+        return node
+
+    def insertBefore(self, new_value, value):
+        """add a new node with the new_value imediately before the first node with the value"""
+        try:
+            node = Node(value)
+        except TypeError:
+            raise TypeError('Cannot insert a value of none')
+
+        current = self.head
+
+        while current.val != value:
+            new_next = current
+            current = current._next
+        node._next = current
+        current = new_next
+        current._next = node
+        return node
+
+    def insert_after(self, new_value, value):
+        """add a new node with the new_value imediately after the first node with the value"""
+        try:
+            node = Node(value)
+        except TypeError:
+            raise TypeError('Cannot insert a value of none')
+
+        current = self.head
+
+        while current.val != value:
+            current = current._next
+        node._next = current._next
+        current._next = node
+        return node
+
+    def remove(self):
+        """remove the node at the head of the linked_list, decrement the ._size and    return the value"""
+        val = self.head
+        self.head = self.head._next
+        self._size -= 1
+        return val
