ccc6ad8f29192797d49d7576fd72c555a21e18cc
Steven Starwalt <steven.starwalt@yahoo.com>
2018-04-25 23:18:43 -0700

    added and new fixture

--- k_tree/conftest.py
+++ k_tree/conftest.py
@@ -17,3 +17,17 @@ def small_k_tree():
     k.insert(7, 2)
     k.insert(8, 3)
     return k
+
+@pytest.fixture
+def repeat_k_tree():
+    """Make a k-ary tree for pytest."""
+    r = KTree()
+    r.insert(1)
+    r.insert(2, 1)
+    r.insert(3, 1)
+    r.insert(3, 1)
+    r.insert(5, 2)
+    r.insert(6, 2)
+    r.insert(7, 2)
+    r.insert(8, 3)
+    return r
