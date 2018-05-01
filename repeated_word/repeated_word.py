b8fd90406801b41e3d3cbf4a1fba9bcc51f4a6cc
Steven Starwalt <steven.starwalt@yahoo.com>
2018-04-30 18:49:05 -0700

    finished testing

--- repeated_word/repeated_word.py
+++ repeated_word/repeated_word.py
@@ -4,20 +4,20 @@
 def repeated_word(string):
     """Find the first repeated word in a string."""
     word = ''
-    words = [] * len(string)
+    words = [0] * len(string)
     index = -1
-    leters = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
-
+    leters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    # import pdb; pdb.set_trace()
     for char in string:
         if char in leters:
             word += char
-        else:
-            if word in words:
-                repeat = ''
-                for char in word:
-                    repeat += char
-                    return repeat
-            else:
-                index += 1
-                words[index] = word
-                word = ''
+        elif word in words:
+            repeat = ''
+            for char in word:
+                repeat += char
+            return repeat
+        elif len(word) > 0:
+            index += 1
+            words[index] = word
+            word = ''
+    return False
