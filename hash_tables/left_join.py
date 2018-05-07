59cee761d6f0892cbc92967dd2851849af6a433d
Steven Starwalt <steven.starwalt@yahoo.com>
2018-05-07 11:43:23 -0700

    changes to the left_join and added the merge_lists whiteboard image

--- hash_tables/left_join.py
+++ hash_tables/left_join.py
@@ -8,18 +8,12 @@ def left_join(left_hash, right_hash):
     joined = []
 
     for bucket in left_hash.buckets:
-        # print(bucket)
         if bucket.head:
             left_keys = [None, None, None]
-            print('\nadding word:', bucket.head.key)
             left_keys[0] = bucket.head.key
-            print('\tleft val:', bucket.head.val)
             left_keys[1] = bucket.head.val
             if right_hash.get(bucket.head.key):
-                print('\tright val:', right_hash.get(bucket.head.key))
                 left_keys[2] = right_hash.get(bucket.head.key)[0]
             else:
-                print('\tright val: None')
-            print('\n\trow:', left_keys)
             joined.append(left_keys)
     return joined
